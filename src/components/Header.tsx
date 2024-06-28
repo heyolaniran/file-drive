304759d460ed3b76de5e1370dcbfe75eabba85a3
heyolaniran <olaniran.abd@gmail.com>
2024-06-28 15:57:14 +0100

    fix: log in submittion

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,29 @@
 
-import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
+import { OrganizationSwitcher, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import React from 'react'
+import { Button } from './ui/button'
 
 export default function Header() {
   return (
     <div className=''>
         <div className=" items-center container mx-auto justify-between py-2 flex">
           <p className='font-bold text-lg'>File Driver</p> 
-          <div className="flex gap-2">
-            <OrganizationSwitcher /> 
-            <UserButton />
+          <div className=" items-center flex gap-2">
+           
+
+            <SignedIn>
+              <OrganizationSwitcher /> 
+              <UserButton />
+            </SignedIn>
+
+            <SignedOut>
+
+              <SignInButton mode='modal'>
+                <Button className='px-6  rounded-full '> Log in  </Button>
+              </SignInButton>
+
+            </SignedOut>
+           
           </div>
           
         </div>
