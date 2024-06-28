1d2613d878f8dc4dc39bd1f2ffb00aec6c19870c
heyolaniran <olaniran.abd@gmail.com>
2024-06-28 15:26:04 +0100

    feat: scoping file by organization or user id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,60 @@
 'use client'
 import { Button } from "@/components/ui/button";
-import { SignInButton,  SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { SignInButton,  SignOutButton, SignedIn, SignedOut, useOrganization, useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 
 export default function Home() {
 
+  const  organization = useOrganization() ;
+
+  const user = useUser(); 
+  // define organization / user id 
+  let orgId = null ; 
+
+
+  if(organization.isLoaded && user.isLoaded) {
+
+     // if we get an organization id or user id 
+      orgId = organization.organization?.id ??  user.user?.id
+
+  }
+
   const createFile = useMutation(api.files.createFile)
 
-  const files = useQuery(api.files.getFiles) ; 
+  // searching files for organization id or by user id if there is not organization 
+
+  const files = useQuery(api.files.getFiles, orgId  ? { orgId } : 'skip'  ) ; 
  
+ 
+  // create file logic 
+
+  const handleCreateFile = () => {
+
+     if(!orgId) {
+        return ; 
+     }
+
+     createFile({
+      name: 'Hello', 
+      orgId 
+     })
+  }
+
+
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
+      {files?.map((file) => {
+        return <div key={file._id}> {file.name} </div>
+      })}
+
+
+      <Button onClick={handleCreateFile}>
+        Click me
+      </Button>
       
       
     </main>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,17 @@
 
-import { UserButton } from '@clerk/nextjs'
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 export default function Header() {
   return (
     <div className=''>
-        <div className="container mx-auto justify-between py-2 flex">
+        <div className=" items-center container mx-auto justify-between py-2 flex">
           <p className='font-bold text-lg'>File Driver</p> 
-
-          <UserButton />
+          <div className="flex gap-2">
+            <OrganizationSwitcher /> 
+            <UserButton />
+          </div>
+          
         </div>
     </div>
   )
